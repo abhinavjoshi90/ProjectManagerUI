ed82f4fd5b770accf32f1661f4542e9ec49bcd22
Abhinav <abhinavjoshi@live.com>
2018-09-13 23:16:27 +0530

    initial check in - 09132018

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AddprojectComponent } from './UI/addproject/addproject.component';
+import { AddtaskComponent } from './UI/addtask/addtask.component';
+import { AdduserComponent } from './UI/adduser/adduser.component';
+import { ViewtaskComponent } from './UI/viewtask/viewtask.component';
+import { routing } from 'src/app/app.router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddprojectComponent,
+    AddtaskComponent,
+    AdduserComponent,
+    ViewtaskComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
