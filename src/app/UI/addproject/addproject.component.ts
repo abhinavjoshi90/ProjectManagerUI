162bce93ec66cbe26e6aa28f6a53349bbb1c1b73
Abhinav <abhinavjoshi@live.com>
2018-09-15 16:47:16 +0530

    Moved Model folder above

--- src/app/UI/addproject/addproject.component.ts
+++ src/app/UI/addproject/addproject.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Project } from '../Model/Project';
 import {DatePipe} from '@angular/common';
+import { Project } from '../../Model/Project';
 //declare var $:any;
 @Component({
     selector: 'app-addproject',
