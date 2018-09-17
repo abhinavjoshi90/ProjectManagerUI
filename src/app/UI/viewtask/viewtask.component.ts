766384f10675b97a252384726a7765851248dec7
Abhinav <abhinavjoshi@live.com>
2018-09-17 11:59:11 +0530

    view task UI changes

--- src/app/UI/viewtask/viewtask.component.ts
+++ src/app/UI/viewtask/viewtask.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./viewtask.component.css']
 })
 export class ViewtaskComponent implements OnInit {
-
+  searchTitle: string;
+  lstItem: string[];
+  selectedprojectName:string;
+  projectName:string;
   constructor() { }
 
   ngOnInit() {
   }
 
+  onPrjSearchClick() {
+    this.searchTitle = "Search Projects";
+    this.lstItem = ["Receivables Edge", "AON Carrier Link", "AON Bridge", "Remit One"];
+  }
+  handleChange(evt) {
+    this.selectedprojectName = evt;
+    console.log(evt);
+  }
+  onSelection() {
+    if (this.selectedprojectName.length > 0) {
+      this.projectName = this.selectedprojectName;
+    }
+  }
 }
